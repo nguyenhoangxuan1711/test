bd5d2d3b809fb2c6fffad05dbe312e9847199589
xuan.nguyen-hoang <xuan.nguyen-hoang@banvien.com.vn>
2025-03-10 13:30:46 +0700

    move test.c to git lfs

--- test.c
+++ test.c
@@ -1,7 +1,3 @@
-#include <stdio.h>
-
-int main(int argc, char const *argv[])
-{
-    printf("hello world!\n");
-    return 0;
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:908e241349d7c1ad23be674df0e8d8fd45c24274f49a04009ff7a22e09c64e27
+size 106
